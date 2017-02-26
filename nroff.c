96594b33a4e185e702cf0135eb580874943745a7
kristapsdz <kristaps@bsd.lv>
2017-02-26 15:58:05 +0000

    Use config.h for all files and pull in real reallocarray.

--- nroff.c
+++ nroff.c
@@ -17,6 +17,8 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include "config.h"
+
 #include <ctype.h>
 #include <err.h>
 #include <stdint.h>
