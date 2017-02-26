d570ea8c9f252eae35ea9c29a6013de6da43d0a8
kristaps <>
2017-02-26 15:58:05 +0000

    Use config.h for all files and pull in real reallocarray.

--- html.c
+++ html.c
@@ -17,6 +17,8 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#include "config.h"
+
 #include <ctype.h>
 #include <stdint.h>
 #include <stdio.h>
