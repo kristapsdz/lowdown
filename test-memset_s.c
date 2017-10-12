762bde0ec985ec4bd3679f25a27093b796821e52
kristaps <>
2017-10-12 00:38:46 +0000

    Use new oconfigure.

--- test-memset_s.c
+++
@@ -1,8 +0,0 @@
-#include <string.h>
-
-int main(void)
-{
-	char buf[10];
-	memset_s(buf, 0, 'c', sizeof(buf));
-	return 0;
-}
--- test-recallocarray.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-int
-main(void)
-{
-	return !recallocarray(NULL, 0, 2, 2);
-}
