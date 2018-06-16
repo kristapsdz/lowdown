b6a3ff9fe1d5fcbd51121198daa53a7763138315
kristapsdz <kristaps@bsd.lv>
2018-06-16 09:10:43 +0000

    Remove debugging, update copy.

--- library.c
+++ library.c
@@ -1,6 +1,6 @@
 /*	$Id$ */
 /*
- * Copyright (c) 2017 Kristaps Dzonsons <kristaps@bsd.lv>
+ * Copyright (c) 2017, 2018 Kristaps Dzonsons <kristaps@bsd.lv>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
