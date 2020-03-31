e69f33d93303305580aaa20602a3f2676b63bd2c
kristapsdz <kristaps@bsd.lv>
2020-03-31 15:11:55 +0000

    Update oconfigure.

--- tests.c
+++ tests.c
@@ -116,6 +116,41 @@ main(void)
 	return(0);
 }
 #endif /* TEST_EXPLICIT_BZERO */
+#if TEST_FTS
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fts.h>
+
+int
+main(void)
+{
+	const char	*argv[2];
+	FTS		*ftsp;
+	FTSENT		*entry;
+
+	argv[0] = ".";
+	argv[1] = (char *)NULL;
+
+	ftsp = fts_open((char * const *)argv,
+	    FTS_PHYSICAL | FTS_NOCHDIR, NULL);
+
+	if (ftsp == NULL)
+		return 1;
+
+	entry = fts_read(ftsp);
+
+	if (entry == NULL)
+		return 1;
+
+	if (fts_set(ftsp, entry, FTS_SKIP) != 0) 
+		return 1;
+
+	if (fts_close(ftsp) != 0)
+		return 1;
+
+	return 0;
+}
+#endif /* TEST_FTS */
 #if TEST_GETEXECNAME
 #include <stdlib.h>
 
