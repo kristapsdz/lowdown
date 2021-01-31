a4933884fdf850695494b202544c1b9a037adf8d
kristapsdz <kristaps@bsd.lv>
2021-01-31 16:13:23 +0000

    More oconfigure.

--- tests.c
+++ tests.c
@@ -593,11 +593,12 @@ TAILQ_HEAD(fooq, foo);
 int
 main(void)
 {
-	struct fooq foo_q;
+	struct fooq foo_q, bar_q;
 	struct foo *p, *tmp;
 	int i = 0;
 
 	TAILQ_INIT(&foo_q);
+	TAILQ_INIT(&bar_q);
 
 	/*
 	 * Use TAILQ_FOREACH_SAFE because some systems (e.g., Linux)
@@ -606,6 +607,10 @@ main(void)
 
 	TAILQ_FOREACH_SAFE(p, &foo_q, entries, tmp)
 		p->bar = i++;
+
+	/* Test for newer macros as well. */
+
+	TAILQ_CONCAT(&foo_q, &bar_q, entries);
 	return 0;
 }
 #endif /* TEST_SYS_QUEUE */
